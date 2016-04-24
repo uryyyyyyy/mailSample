631642264c90dfe58f332cd5770efbe46409c8ea
uryyyyyyy <koki305@gmail.com>
2016-04-24 19:35:13 +0900

    SES

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 
-```
+```shell
 gem install mailcacher
 mailcacher
+
+# set env
+export USER_NAME="your username of SES"
+export PASSWORD="your password of SES"
 
 cd mailSample
 ./activator run
--- mailSample/src/main/java/com/example/Hello.java
+++ mailSample/src/main/java/com/example/Hello.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
@@ -7,17 +8,33 @@ import org.apache.commons.mail.SimpleEmail;
 class Hello {
     public static void main(String[] args) throws EmailException {
         System.out.println("Hello World");
-        sendEmail();
+        sendSESEmail();
     }
 
+    // for mailCacher
     private static void sendEmail() throws EmailException {
 
         Email email = new SimpleEmail();
         email.setHostName("127.0.0.1");
         email.setSmtpPort(1025);
-//        email.setAuthenticator(new DefaultAuthenticator("username", "password"));
-//        email.setSSLOnConnect(true);
         email.setFrom("uryyyyyyy@gmail.com");
+        email.setSubject("TestMail");
+        email.setMsg("This is a test mail ... :-)");
+        email.addTo("koki305@gmail.com");
+        email.send();
+    }
+
+    //for SES
+    private static void sendSESEmail() throws EmailException {
+        String userName = System.getenv("USER_NAME");
+        String password = System.getenv("PASSWORD");
+
+        Email email = new SimpleEmail();
+        email.setHostName("email-smtp.us-east-1.amazonaws.com");
+        email.setSmtpPort(465);
+        email.setAuthenticator(new DefaultAuthenticator(userName, password));
+        email.setSSLOnConnect(true);
+        email.setFrom("koki305@gmail.com");
         email.setSubject("TestMail");
         email.setMsg("This is a test mail ... :-)");
         email.addTo("koki305@gmail.com");
